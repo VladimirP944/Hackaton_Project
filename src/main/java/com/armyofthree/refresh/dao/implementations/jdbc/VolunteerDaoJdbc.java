60642ec0d8757770722519fef5d1222664c119a7
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-15 22:32:22 +0300

    Add constructor for volunteer dao

--- src/main/java/com/armyofthree/refresh/dao/implementations/jdbc/VolunteerDaoJdbc.java
+++ src/main/java/com/armyofthree/refresh/dao/implementations/jdbc/VolunteerDaoJdbc.java
@@ -1,11 +1,24 @@
 package com.armyofthree.refresh.dao.implementations.jdbc;
 
 import com.armyofthree.refresh.dao.VolunteerDao;
+import com.armyofthree.refresh.models.users.DisplacedPerson;
 import com.armyofthree.refresh.models.users.Volunteer;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class VolunteerDaoJdbc implements VolunteerDao {
+
+    private DataSource dataSource;
+
+    public VolunteerDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public ArrayList<Volunteer> getAll() {
         return null;
