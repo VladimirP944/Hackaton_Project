d301c86c7f70dbaef513848da4c20bcd7800e5b2
dragosvasile11 <vasile.dragos11@gmail.com>
2022-04-16 00:00:47 +0300

    Create functions for login & register.

--- src/main/flask/main.py
+++ src/main/flask/main.py
@@ -16,6 +16,70 @@ app.secret_key = 'coolHack2'
 load_dotenv()
 
 
+@app.route("/", methods=['GET', 'POST'])
+def welcome_user():
+    if "username" not in session:
+        return render_template("welcome.html")
+    return render_template("welcome.html", user=session["username"].split('@')[0].capitalize())
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        username = request.form.get('username')
+
+        if not (queries.check_if_user_exists(username)['exists']):
+            password = request.form.get('password')
+            hashed_password = generate_password_hash(password)
+            queries.add_user(username, hashed_password)
+            return redirect(url_for("welcome_user"))
+        flash('User already exists')
+
+    return redirect(url_for("welcome_user"))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def check_user_credentials():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if bool(queries.check_if_user_exists(username)['exists']):
+
+            hashed_password = queries.get_user_password(username)["password"]
+            if check_password_hash(hashed_password, password):
+                session['username'] = username
+                session['user_id'] = (queries.get_user_id(username))['id']
+                return redirect(url_for('welcome_user'))
+            else:
+                flash('Wrong password')
+        else:
+            flash('User does not exist')
+    return redirect(url_for("welcome_user"))
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("welcome_user"))
+
+
+@app.route('/delete_user')
+def delete_user():
+    queries.delete_user(queries.get_user_id(session['username'])['id'])
+    session.clear()
+    flash('Account Deleted')
+    return redirect(url_for("welcome_user"))
+
+
+@app.route("/api/check-if-user-logged-in")
+@json_response
+def check_logged_in():
+    if 'username' in session:
+        return {"response": True}
+    return {"response": False}
+
+
 def main():
     app.run(debug=True)
 
