68d5ebab7eae4e7b233bf08db2386442b1a6774c
DeDe88&*&@ <sebastian.techera015@gmail.com>
2022-04-15 18:25:04 +0100

    Implemented constructors, getters and setters for User classes.

--- src/main/java/com/armyofthree/refresh/models/users/Admin.java
+++ src/main/java/com/armyofthree/refresh/models/users/Admin.java
@@ -1,4 +1,7 @@
 package com.armyofthree.refresh.models.users;
 
 public class Admin extends User {
+    public Admin(String name, String address, String email, String phone, String profilePhoto, String password) {
+        super(name, address, email, phone, UserType.ADMIN, profilePhoto, password);
+    }
 }
--- src/main/java/com/armyofthree/refresh/models/users/DisplacedPerson.java
+++ src/main/java/com/armyofthree/refresh/models/users/DisplacedPerson.java
@@ -10,4 +10,66 @@ public class DisplacedPerson extends User {
     private boolean lookingToSellProducts;
     private String documents;
     private boolean pets;
+
+    public DisplacedPerson(String name, String address, String email, String phone, UserType type, String profilePhoto, int numberOfPeople, String documents, String password) {
+        super(name, address, email, phone, UserType.DISPLACED_PERSON, profilePhoto, password);
+        this.numberOfPeople = numberOfPeople;
+        this.documents = documents;
+    }
+
+    public boolean isLookingForJob() {
+        return lookingForJob;
+    }
+
+    public void setLookingForJob(boolean lookingForJob) {
+        this.lookingForJob = lookingForJob;
+    }
+
+    public boolean isLookingForShelter() {
+        return lookingForShelter;
+    }
+
+    public void setLookingForShelter(boolean lookingForShelter) {
+        this.lookingForShelter = lookingForShelter;
+    }
+
+    public boolean isLookingForBusinessLocation() {
+        return lookingForBusinessLocation;
+    }
+
+    public void setLookingForBusinessLocation(boolean lookingForBusinessLocation) {
+        this.lookingForBusinessLocation = lookingForBusinessLocation;
+    }
+
+    public boolean isLookingForFood() {
+        return lookingForFood;
+    }
+
+    public void setLookingForFood(boolean lookingForFood) {
+        this.lookingForFood = lookingForFood;
+    }
+
+    public boolean isLookingForMedicalAssistance() {
+        return lookingForMedicalAssistance;
+    }
+
+    public void setLookingForMedicalAssistance(boolean lookingForMedicalAssistance) {
+        this.lookingForMedicalAssistance = lookingForMedicalAssistance;
+    }
+
+    public boolean isLookingToSellProducts() {
+        return lookingToSellProducts;
+    }
+
+    public void setLookingToSellProducts(boolean lookingToSellProducts) {
+        this.lookingToSellProducts = lookingToSellProducts;
+    }
+
+    public boolean hasPets() {
+        return pets;
+    }
+
+    public void setPets(boolean pets) {
+        this.pets = pets;
+    }
 }
