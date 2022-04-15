452de0dbc902c9bec3773de697e0335016b06c44
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-15 21:55:30 +0300

    Remove address from user

--- src/main/java/com/armyofthree/refresh/models/users/User.java
+++ src/main/java/com/armyofthree/refresh/models/users/User.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 public abstract class User {
     private String name;
-    private String address;
     private String email;
     private String phone;
     private UUID id;
@@ -12,9 +11,8 @@ public abstract class User {
     private String profilePhoto;
     private String password;
 
-    public User(String name, String address, String email, String phone, UserType type, String profilePhoto, String password) {
+    public User(String name, String email, String phone, UserType type, String profilePhoto, String password) {
         this.name = name;
-        this.address = address;
         this.email = email;
         this.phone = phone;
         this.type = type;
