59cc3c18ed4aead0fd05049e55d912b2c84b265c
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-15 21:58:24 +0300

    Add get one displaced person method

--- src/main/java/com/armyofthree/refresh/dao/implementations/jdbc/DisplacedPersonDaoJdbc.java
+++ src/main/java/com/armyofthree/refresh/dao/implementations/jdbc/DisplacedPersonDaoJdbc.java
@@ -57,7 +57,38 @@ public class DisplacedPersonDaoJdbc implements DisplacedPersonDao {
 
     @Override
     public DisplacedPerson getDisplacedPerson(String username) {
-        return null;
+        try(Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM applicants WHERE username = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, username);
+            ResultSet rs = st.executeQuery();
+
+            if (!rs.next()) {
+                return null;
+            }
+
+            String email = rs.getString(1);
+            String password =rs.getString(2);
+            String profilePhoto = rs.getString(3);
+            String name = rs.getString(4);
+            int numberOfPeople = rs.getInt(5);
+            String phone = rs.getString(6);
+            String documents = rs.getString(7);
+            Boolean appliedForHome = rs.getBoolean(8);
+
+            DisplacedPerson person = new DisplacedPerson(name,
+                    email,
+                    phone,
+                    profilePhoto,
+                    numberOfPeople,
+                    documents,
+                    password);
+
+            person.setAppliedForHome(appliedForHome);
+            return person;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
