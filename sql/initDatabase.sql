2068fb4db6c968615c765185f6cefeb71118759d
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-16 11:40:51 +0300

    Change database tables

--- sql/initDatabase.sql
+++ sql/initDatabase.sql
@@ -1,16 +1,12 @@
-ALTER TABLE IF EXISTS ONLY services DROP CONSTRAINT IF EXISTS fk_service_home_owner_iduser_id CASCADE;
-ALTER TABLE IF EXISTS ONLY services DROP CONSTRAINT IF EXISTS fk_applicant_id CASCADE;
-ALTER TABLE IF EXISTS ONLY locations DROP CONSTRAINT IF EXISTS fk_home_owner_id CASCADE;
-ALTER TABLE IF EXISTS ONLY applicants DROP CONSTRAINT IF EXISTS fk_home_id CASCADE;
+ALTER TABLE IF EXISTS ONLY services DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY locations DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
 
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS services;
-DROP TABLE IF EXISTS applicants;
-DROP TABLE IF EXISTS home_owners;
-DROP TABLE IF EXISTS admins;
+DROP TABLE IF EXISTS users;
 
 
-CREATE TABLE applicants (
+CREATE TABLE users (
     id serial NOT NULL,
     username VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
@@ -22,27 +18,14 @@ CREATE TABLE applicants (
     documents VARCHAR,
     applied_for_home boolean DEFAULT false NOT NULL,
     home_id INTEGER,
+    identity_card VARCHAR,
     PRIMARY KEY (id)
-);
-
-
-CREATE TABLE home_owners (
-     id serial NOT NULL,
-     username VARCHAR UNIQUE NOT NULL,
-     password VARCHAR NOT NULL,
-     photo_id VARCHAR,
-     type VARCHAR,
-     phone_number VARCHAR,
-     name VARCHAR,
-     photo_criminal_record VARCHAR,
-     identity_card VARCHAR,
-     PRIMARY KEY (id)
 );
 
 
 CREATE TABLE locations (
    id serial NOT NULL,
-   home_owner_id INTEGER NOT NULL,
+   user_id INTEGER NOT NULL,
    address VARCHAR UNIQUE NOT NULL,
    available_space INTEGER NOT NULL,
    total_space INTEGER NOT NULL,
@@ -55,7 +38,7 @@ CREATE TABLE locations (
 
 CREATE TABLE services (
   id serial NOT NULL,
-  home_owner_id INTEGER,
+  user_id INTEGER NOT NULL,
   applicant_id INTEGER,
   education boolean,
   pets boolean,
@@ -66,22 +49,8 @@ CREATE TABLE services (
 );
 
 
-CREATE TABLE admins (
-    id serial NOT NULL,
-    username VARCHAR UNIQUE NOT NULL,
-    password VARCHAR NOT NULL,
-    PRIMARY KEY (id)
-);
-
-
 ALTER TABLE ONLY services
-    ADD CONSTRAINT fk_service_home_owner_id FOREIGN KEY (home_owner_id) REFERENCES home_owners(id);
-
-ALTER TABLE ONLY services
-    ADD CONSTRAINT fk_applicant_id FOREIGN KEY (applicant_id) REFERENCES applicants(id);
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
 ALTER TABLE ONLY locations
-    ADD CONSTRAINT fk_home_owner_id FOREIGN KEY (home_owner_id) REFERENCES home_owners(id);
-
-ALTER TABLE ONLY applicants
-    ADD CONSTRAINT fk_home_id FOREIGN KEY (home_id) REFERENCES locations(id);
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
