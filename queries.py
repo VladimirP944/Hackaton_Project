b34a9da938c2b806ced723ae80b7acb281c5a6f7
Vlad Puschiulescu <vladpuschiulescu@gmail.com>
2022-04-16 15:37:55 +0300

    Add query to get all locations

--- queries.py
+++ queries.py
@@ -110,12 +110,13 @@ def get_user_type(username):
     return data_manager.execute_select(query, {'username': username}, fetchall=False)
 
 
-def add_location(name, user_id, address, available_space, total_space, photo_id):
+def add_location(name, description, user_id, address, available_space, total_space, photo_id):
     query = """
             INSERT INTO locations VALUES
                 (DEFAULT, %(name)s, %(user_id)s, %(address)s, %(available_space)s, %(total_space)s, %(photo_id)s)
             """
     data_manager.execute_select(query, {"name": name,
+                                        "description": description,
                                         "user_id": user_id,
                                         "address": address,
                                         "available_space": available_space,
@@ -123,3 +124,13 @@ def add_location(name, user_id, address, available_space, total_space, photo_id)
                                         "photo_id": photo_id,
                                         },
                                 select=False)
+
+
+def get_all_locations():
+
+    query = '''
+            SELECT *
+            FROM locations;
+            '''
+
+    return data_manager.execute_select(query, fetchall=True)
